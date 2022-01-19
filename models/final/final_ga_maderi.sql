b22493431261cb2435cf69647f7b2ecbaf963072
Gayeong Lee <yonggalee@datamarketing.co.kr>
2022-01-19 09:29:09 +0000

    final model for cdp-develop db

--- models/final/final_ga_maderi.sql
+++ models/final/final_ga_maderi.sql
@@ -20,5 +20,53 @@ select
     a.geo_region,
     a.geo_city,
     a.match_user_id,
-
-from {{ref('final_ga')}} a left join 
+    b.accounts_user_id,
+    b.accounts_user_password,
+    b.accounts_user_last_login,
+    b.accounts_user_email,
+    b.accounts_user_first_name,
+    b.accounts_user_last_name,
+    b.accounts_user_is_active,
+    b.accounts_user_is_staff,
+    b.accounts_user_is_admin,
+    b.accounts_user_is_superuser,
+    b.accounts_user_date_joined,
+    b.accounts_user_created_at,
+    b.accounts_user_updated_at,
+    b.accounts_user_sns_type,
+    b.accounts_profile_id,
+    b.accounts_profile_company,
+    b.accounts_profile_team,
+    b.accounts_profile_position,
+    b.accounts_profile_category,
+    b.accounts_profile_license,
+    b.accounts_profile_phone_number,
+    b.accounts_profile_marketing_info,
+    b.accounts_profile_private_info,
+    b.accounts_profile_service_info,
+    b.accounts_profile_level,
+    c.t_payment_user_license_license as t_pyament_user_license,
+    c.t_payment_user_license_status,
+    d.t_user_keyword_keyword_name as t_user_keyword_name,
+    d.t_user_keyword_synonym,
+    d.t_user_keyword_exclusion_word,
+    d.t_user_keyword_created_at,
+    d.t_user_keyword_is_own,
+    e.t_ch_user_data_ch_id,
+    e.t_ch_user_data_is_own,
+    e.t_ch_user_data_sns_type,
+    e.t_ch_user_data_created_at,
+    f.t_payment_license_table_price as t_payment_license_price,
+    g.accounts_loginhistory_created_at
+from {{ref('final_ga')}} a  left join {{ref('stg_accounts')}} b
+                                on a.user_id = b.user_id
+                            left join {{ref('stg_payment_user_license')}} c 
+                                on a.user_id = c.user_id
+                            left join {{ref('stg_user_keyword')}} d
+                                on a.user_id = d.user_id
+                            left join {{ref('stg_user_ch')}} e 
+                                on a.user_id = e.user_id
+                            left join {{ref('stg_payment_license_info')}} f 
+                                on c.t_payment_user_license_license = f.t_payment_license_table_license
+                            left join {{ref('stg_accounts_history')}} g 
+                                on a.user_id = g.user_id
