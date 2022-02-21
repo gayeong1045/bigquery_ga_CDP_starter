98eca547384bb75821d3c5a19bdf237c16986cd2
Gayeong Lee <yonggalee@datamarketing.co.kr>
2022-02-21 08:52:28 +0000

    conversion_table_set

--- models/flat_ga/flat_ga_events.sql
+++ models/flat_ga/flat_ga_events.sql
@@ -12,7 +12,10 @@
 
 SELECT
   event_id,
+  event_date,
   event_name,
+  user_id,
+  user_pseudo_id,
   params.key as event_params_key,
   params.value.string_value as event_params_value_string,
   params.value.int_value as event_params_value_int,
