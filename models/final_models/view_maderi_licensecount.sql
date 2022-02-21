3b3503da8017928af02871813cb01326bcf3a404
Gayeong Lee <yonggalee@datamarketing.co.kr>
2022-02-21 05:34:21 +0000

    incremental_test

--- models/final_models/view_maderi_licensecount.sql
+++ models/final_models/view_maderi_licensecount.sql
@@ -1,3 +1,5 @@
+-- depends_on: {{ ref('stg_maderi_accounts') }}
+
 {{
     config(
         materialized='incremental',
@@ -10,7 +12,7 @@ select
 from {{ref('cal_accounts_activeuser')}}
 {% if is_incremental() %}
 
-where today >= (select max(cast(synced_date as date)) from {{ref('stg_maderi_accounts')}})
+where today >= (select max(cast(synced_time as date)) from {{ref('stg_maderi_accounts')}})
 
 {% endif %}
 
