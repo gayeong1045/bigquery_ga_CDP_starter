b2a1275541fe38035f16ed959902104dbfd86875
Gayeong Lee <dataconsulting@datamarketing.co.kr>
2022-05-27 09:33:42 +0000

    고객사 type 추가

--- models/final_models/view_maderi_accountpayment.sql
+++ models/final_models/view_maderi_accountpayment.sql
@@ -11,6 +11,7 @@ select
     sa.accounts_user_is_active,
     sa.accounts_profile_company,
     sa.accounts_profile_license,
+    sa.accounts_user_email,
     cast(ph.t_payment_history_event_at as datetime) as t_payment_history_event_at,
     ph.t_payment_history_amount,
     CASE
@@ -27,9 +28,10 @@ contract_data as (
         true as accounts_user_is_active,
         company as accounts_profile_company,
         license as accounts_profile_license,
+        concat('계약서_', company) as accounts_user_email,
         cast(date as datetime ) as t_payment_history_event_at,
         price as t_payment_history_amount,
-        'cusomer' as is_employee
+        'customer' as is_employee
     from {{ref('stg_gsheet_contractpay')}}
 ),
 
@@ -37,6 +39,19 @@ union_data as (
     select * from online_data
     union all
     select * from contract_data
+),
+
+new_old as (
+    select 
+        *,
+        case
+            when pay_cnt = 1 then '신규'
+            else '기존' end as type 
+    from 
+        (select 
+            *,
+            row_number () over (partition by accounts_user_email) as pay_cnt
+        from union_data)
 )
 
-select * from union_data
+select * from new_old
